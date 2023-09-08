46e24e7e961e6dd452620a4f5062fe8818f6dcaa
838861hs <philoaki0@gmail.com>
2023-09-08 10:50:39 +0900

    jsを外部ファイル化

--- fig.js
+++ fig.js
@@ -1,89 +1,8 @@
-let backgroundCanvas = document.getElementById('backgroundCanvas');
-let backgroundContext = backgroundCanvas.getContext('2d');
-let canvas = document.getElementById('myCanvas');
-let context = canvas.getContext('2d');
 
-let isBackgroundEditMode = false;
-let isEraser = false;
-let isDrawing = false;
-let lastX, lastY;
-
-let dotX = 400; // 点の初期X座標
-let dotY = 270; // 点の初期Y座標（青い線の中心から2マス上）
-let draggingDot = null;
-
-document.getElementById('toggleEraser').addEventListener('click', function() {
-  isEraser = !isEraser;
-  this.innerText = isEraser ? 'ペンモード' : '消しゴムモード';
-});
-
-document.getElementById('toggleMode').addEventListener('click', function() {
-  isBackgroundEditMode = !isBackgroundEditMode;
-  this.innerText = isBackgroundEditMode ? '絵描きモード' : '背景編集モード';
-});
-
-canvas.addEventListener('mousedown', function(e) {
-  isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-  draw(e);
-});
-
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', endDrawing);
-canvas.addEventListener('mouseout', endDrawing);
-
-function draw(e) {
-  if(!isDrawing) return;
-
-  if(isBackgroundEditMode) {
-    let x = e.offsetX;
-    let y = e.offsetY;
-
-    if(e.type === 'mousedown') {
-      if(Math.abs(x - dotX) < 5 && Math.abs(y - dotY) < 5) {
-        draggingDot = { x: dotX, y: dotY };
-      }
-    } else if(e.type === 'mousemove' && draggingDot) {
-      dotX = x;
-      dotY = y;
-      redrawBackground();
-    } else if(e.type === 'mouseup' || e.type === 'mouseout') {
-      draggingDot = null;
-    }
-
-    return;
-  }
-
-  context.lineWidth = isEraser ? 10 : 5;
-  context.lineCap = 'round';
-  context.globalCompositeOperation = isEraser ? 'destination-out' : 'source-over';
-  
-  context.beginPath();
-  context.moveTo(lastX, lastY);
-  context.lineTo(e.offsetX, e.offsetY);
-  context.stroke();
-
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-}
-
-function endDrawing() {
-  isDrawing = false;
-  draggingDot = null;
-}
-
-function redrawBackground() {
-  backgroundContext.clearRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
-  drawGrid();
-  drawBlueLine();
-  drawDot();
-}
-
-function drawDot() {
-  backgroundContext.fillStyle = 'red'; 
-  backgroundContext.beginPath();
-  backgroundContext.arc(dotX, dotY, 5, 0, 2 * Math.PI); 
-  backgroundContext.fill();
-}
+var backgroundCanvas = document.getElementById('backgroundCanvas');
+var backgroundContext = backgroundCanvas.getContext('2d');
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
 
 function drawGrid() {
   backgroundContext.strokeStyle = '#e0e0e0';
@@ -102,4 +21,68 @@ function drawGrid() {
 
   backgroundContext.stroke();
 }
-// drawGrid と drawBlueLine 関数はここに追加します
+
+function drawBlueLine() {
+var centerX = backgroundCanvas.width / 2;
+var centerY = backgroundCanvas.height / 2;
+var gridSpacing = 30;
+var lineLength = gridSpacing * 6;
+
+var adjustedCenterX = Math.round(centerX / gridSpacing) * gridSpacing;
+
+backgroundContext.strokeStyle = 'blue';
+backgroundContext.lineWidth = 2;
+
+backgroundContext.beginPath();
+backgroundContext.moveTo(adjustedCenterX - lineLength / 2, centerY);
+backgroundContext.lineTo(adjustedCenterX + lineLength / 2, centerY);
+backgroundContext.stroke();
+}
+function drawDot() {
+var centerX = backgroundCanvas.width / 2;
+var centerY = backgroundCanvas.height / 2;
+var gridSpacing = 30;
+
+var adjustedCenterX = Math.round(centerX / gridSpacing) * gridSpacing;
+
+backgroundContext.fillStyle = 'red'; // 点の色を赤に設定します
+backgroundContext.beginPath();
+backgroundContext.arc(adjustedCenterX, centerY - gridSpacing * 2, 5, 0, 2 * Math.PI); // 点の半径を5に設定します
+backgroundContext.fill();
+}
+
+drawGrid();
+drawBlueLine();
+drawDot();
+
+var isEraser = false;
+var isDrawing = false;
+var lastX, lastY;
+
+document.getElementById('toggleEraser').addEventListener('click', function() {
+  isEraser = !isEraser;
+  this.innerText = isEraser ? 'ペンモード' : '消しゴムモード';
+});
+
+canvas.addEventListener('mousedown', function(e) {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+function draw(e) {
+  if(!isDrawing) return;
+  context.lineWidth = isEraser ? 10 : 5;
+  context.lineCap = 'round';
+  context.globalCompositeOperation = isEraser ? 'destination-out' : 'source-over';
+  
+  context.beginPath();
+  context.moveTo(lastX, lastY);
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
